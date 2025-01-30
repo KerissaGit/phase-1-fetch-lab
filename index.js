60793db335834544deea85bbdbcd32b07c016ad6
KerissaGit <kerissa525@gmail.com>
2025-01-30 13:22:18 -0500

    2 Passing

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-//Trying to define fetchbooks but keep having a brain fart.
-//how do i define fetchbooks? Am I in the right location...
-// // const fetchBooks = document.querySelector('#books')
-
-// const fetchBooks = async () => {
-//   try {
-//     const response = await fetch('https://anapioficeandfire.com/api/books')
-//     if (!response.ok){
-//       throw new Error('error')
-//     }
-//     const data = await response.json() 
-//     renderBooks(data) //why dont i work? wrong location?
-//     return data
-//   } catch (error){
-//     alert (error)
-//   }
-//   // renderBooks(data) //Do i go here instead? Neither worked
-//   // To pass the tests, don't forget to return your fetch! 
-// }
-
 const fetchBooks = () => {
   return fetch('https://anapioficeandfire.com/api/books')
   .then(response => response.json())
@@ -26,7 +6,6 @@ const fetchBooks = () => {
 }
 
 
-//functions were provided below
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
